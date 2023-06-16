955ec810eb2a85eff56f66ab9b9814cb7761ff7b
murrdis <dias.murzagaliyev@nu.edu.kz>
2023-06-17 02:03:57 +0600

    HomeWork1

---
+++ To_do_list_Yandex/Service/FileCache.swift
@@ -0,0 +1,108 @@
+import Foundation
+
+class FileCache {
+    private(set) var todoItems: [TodoItem] = []
+    
+    
+    var allTodoItems: [TodoItem] {
+        return todoItems
+    }
+    
+    private func addTodoItem(_ item: TodoItem) {
+        if let existingItemIndex = todoItems.firstIndex(where: { $0.id == item.id }) {
+            todoItems[existingItemIndex] = item
+        } else {
+            todoItems.append(item)
+        }
+    }
+    
+    private func removeTodoItem(withID id: String) {
+        todoItems.removeAll { $0.id == id }
+    }
+    
+    func saveJsonToFile(_ fileName: String) {
+        
+        let filePath = Constants.documentDirectory.appendingPathComponent(fileName)
+        
+        
+        
+        do {
+            let jsonArray = todoItems.map { $0.json } as? [[String : Any]]
+            let jsonData = try JSONSerialization.data(withJSONObject: jsonArray!, options: .prettyPrinted)
+
+            try jsonData.write(to: filePath)
+            print("Data saved to json file: \(filePath)")
+        } catch {
+            // Нужно кидать ошибки
+            print("Failed to save data to json file: \(filePath). Error: \(error)")
+        }
+    }
+    
+    func loadJsonFromFile(_ fileName: String) {
+
+        let filePath = Constants.documentDirectory.appendingPathComponent(fileName)
+        
+        do {
+            let jsonData = try Data(contentsOf: filePath)
+            let jsonObject = try JSONSerialization.jsonObject(with: jsonData) as? [[String : Any]]
+            
+            if let jsonArray = jsonObject {
+                todoItems = jsonArray.compactMap { TodoItem.parse(json: $0) }
+            }
+            
+            print("Data loaded from json file: \(filePath)")
+        } catch {
+            print("Failed to load data from json file: \(filePath). Error: \(error)")
+        }
+    }
+    
+    
+    func saveCsvToFile(_ fileName: String) {
+        
+        let filePath = Constants.documentDirectory.appendingPathComponent(fileName)
+
+        let csvString = todoItems.map{ $0.csv }.joined(separator: "\n")
+        
+        do {
+            try csvString.write(to: filePath, atomically: true, encoding: .utf8)
+            print("Data saved to csv file: \(filePath)")
+        } catch {
+            print("Failed to save data to csv file: \(filePath). Error: \(error)")
+        }
+    }
+    
+    func loadCsvFromFile(_ fileName: String) {
+        
+        let filePath = Constants.documentDirectory.appendingPathComponent(fileName)
+
+        do {
+            let csvString = try String(contentsOf: filePath, encoding: .utf8)
+            
+            let csvLines = csvString.components(separatedBy: "\n")
+            
+            var loadedItems: [TodoItem] = []
+            
+            for line in csvLines {
+                if let item = TodoItem.parse(csv: line) {
+                    loadedItems.append(item)
+                }
+            }
+            
+            todoItems = loadedItems
+            
+            print("Data loaded from csv file: \(filePath)")
+        } catch {
+            print("Failed to load data from csv file: \(filePath). Error: \(error)")
+        }
+    }
+}
+
+
+
+struct Constants {
+    static let documentDirectory = FileManager.default.urls(
+        for: .documentDirectory,
+        in: .userDomainMask
+    ).first!
+
+}
---
+++ To_do_list_Yandex/ViewController.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+//        let oneItem = TodoItem(id: "123", text: "hello", importance: .normal, deadline: Date(), isCompleted: false, creationDate: Date(), modificationDate: nil)
+//        print(oneItem.json)
+//        if let item  = TodoItem.parse(json: oneItem.json) {
+//            print(item)
+//        }
+//
+//        let manager = FileManager.default
+//
+//        guard let url = manager.urls(
+//            for: .documentDirectory,
+//            in: .userDomainMask
+//        ).first else {
+//            return
+//        }
+//        print(url)
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
